a8dabe3cfcf5cc497442be9b7c92826716391a4f
rithu <contactrithuraj@gmail.com>
2026-03-21 17:30:00 +0530

    inital commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM eclipse-temurin:17-jdk 
+
+
+WORKDIR /app
+
+COPY target/ci-cd-devops-1.0-SNAPSHOT.jar app.jar
+
+CMD ["java","-jar","app.jar"]
+
+
---
+++ src/main/java/com/rithu/devops/App.java
@@ -0,0 +1,13 @@
+package com.rithu.devops;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("🚀 CI/CD DevOps Project Running");
+        System.out.println("Addition: " + add(2, 3));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
